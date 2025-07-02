d88423b4ee48f181af489c1d746dd97e654dfcab
Mr. Vadsalasack VORLAVONGSOR <tik.github@gmail.com>
2025-07-02 16:02:25 +0700

    Fix: Conflict IOS setting

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,20 +1,13 @@
 import Flutter
 import UIKit
-<<<<<<< HEAD
 import GoogleMaps
-=======
-
->>>>>>> 0d04b9071e082b16868912fd964bd2e2d6fdcf95
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-<<<<<<< HEAD
     GMSServices.provideAPIKey("AIzaSyDk53-BW6_sg81G_Du84WfFnC3BUJBHXwI")
-=======
->>>>>>> 0d04b9071e082b16868912fd964bd2e2d6fdcf95
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
